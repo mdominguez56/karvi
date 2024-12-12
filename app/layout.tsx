eb541275b88dc87b43b9d7d962209b857bd3e1c0
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2024-12-12 16:44:28 -0300

    feat: add meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,33 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Compra de Carros Usados e Novos com Historico Veicular | Karvi",
-  description: "Encontre os melhores carros usados e novos no Karvi.com.br! Confira nosso historico veicular detalhado e faça uma compra segura. Descubra ofertas incriveis e adquira seu carro ideal.",
+  title: "Compra de Carros Usados e Novos com Histórico Veicular | Karvi",
+  description:
+    "Encontre os melhores carros usados e novos no Karvi.com.br! Confira nosso histórico veicular detalhado e faça uma compra segura. Descubra ofertas incríveis e adquira seu carro ideal.",
+  keywords: ["carros usados", "carros novos", "histórico veicular", "compra segura", "Karvi"],
+  authors: [{ name: "Karvi Team", url: "https://www.karvi.com.br/" }],
+  publisher: "Karvi",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: "Compra de Carros Usados e Novos com Histórico Veicular | Karvi",
+    description:
+      "Encontre os melhores carros usados e novos no Karvi.com.br! Confira nosso histórico veicular detalhado e faça uma compra segura. Descubra ofertas incríveis e adquira seu carro ideal.",
+    url: "https://www.karvi.com.br/",
+    locale: "pt_BR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Compra de Carros Usados e Novos com Histórico Veicular | Karvi",
+    description:
+      "Encontre os melhores carros usados e novos no Karvi.com.br! Confira nosso histórico veicular detalhado e faça uma compra segura. Descubra ofertas incríveis e adquira seu carro ideal.",
+  },
+  alternates: {
+    canonical: "https://www.karvi.com.br/",
+  },
 };
 
 export default function RootLayout({
@@ -12,7 +37,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
+      <head>
+        <meta name="author" content="Karvi Team" />
+        <meta name="publisher" content="Karvi" />
+        <meta name="robots" content="INDEX,FOLLOW" />
+      </head>
       <body className="antialiased bg-white text-gray-900">
         <main className="container mx-auto p-4">{children}</main>
       </body>
